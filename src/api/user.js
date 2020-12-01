881830ec8a953e2ba79a4e07601ab036d8e002aa
Ayaka Neko <neko@ayaka.moe>
2020-12-01 19:13:31 +0800

    :sparkles: Added api token to restrict access
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,30 @@
 const KoaRouter = require('koa-router')
+const config = require('../../config.json')
 
 const { getUserInfo, getUnbinding, getSetAvailable, getUserInfoPlain, getUserInfoList, postUserInfoList } = require('../controllers/user')
 
 let userRouters = new KoaRouter()
+
+userRouters.use(async (ctx, next) => {
+    const method = ctx.method
+    if (method === 'GET') {
+        if (!ctx.request.query) {
+            return
+        }
+        else if (ctx.request.query !== config.apiKey) {
+            return
+        }
+    }
+    else {
+        if (!ctx.request.body.apiKey) {
+            return
+        }
+        else if (ctx.request.body.apiKey !== config.apiKey) {
+            return
+        }
+    }
+    await next()
+})
 
 userRouters.get("/unbinding", getUnbinding)
 userRouters.get("/setAvailable", getSetAvailable)

39c5cd930823bb50546bfcca8a5fa911cd5fdfd0
Ayaka Neko <neko@ayaka.moe>
2020-12-01 16:15:31 +0800

    :sparkles: support more params with redirection
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,7 +4,12 @@ const Log = require('../util/log')
 let goToAuthPage = async (ctx, next) => {
     Log.info('Auth Request Redirected to ' + ctx.globalConfig.publicUrl + '/auth/' + ctx.params.platform)
     ctx.status = 302
-    ctx.redirect(ctx.globalConfig.publicUrl + '/auth/' + ctx.params.platform)
+    if (ctx.request.query.token) {
+        ctx.redirect(ctx.globalConfig.publicUrl + '/auth/' + ctx.params.platform + '?token=' + ctx.request.query.token + '&network=' + ctx.request.query.network)
+    }
+    else {
+        ctx.redirect(ctx.globalConfig.publicUrl + '/auth/' + ctx.params.platform)
+    }
     await next()
 }
 

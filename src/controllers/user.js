f0b5854ac779cccb96caed3d3cf469667faeb0c9
Ayaka Neko <neko@ayaka.moe>
2020-12-01 22:20:21 +0800

    :sparkles: 重新构建接口结构和安全策略
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,21 +1,37 @@
 const Log = require('../util/log')
 const Store = require("../store/store")
 
+const { disassemble } = require('../util/cookie')
+
 let getUserInfo = async (ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
+
+    const accessToken = ctx.request.headers['x-access-token']
+    if (accessToken) {
+        ctx.user = disassemble(accessToken)
+    }
+
+    if (!ctx.user || !ctx.user.id) {
+        ctx.status = 403
+        return
+    }
+
+    ctx.user.id = ctx.user.id + ''
+
     let profile
-    
     if (query.platform) {
-        profile = await Store.user.findOne({ key: 'User' + query.platform + 'Profile', id: ctx.params.id })
+        profile = await Store.user.findOne({ key: 'User' + query.platform + 'Profile', id: ctx.user.id })
     }
     else {
-        profile = await Store.user.findOne({ key: 'UserProfile', id: ctx.params.id })
+        profile = await Store.user.findOne({ key: 'UserProfile', id: ctx.user.id })
     }
     
     if (!profile) ctx.body = { code: 0, message: 'User Not Found' }
     else {
         if (query.platform) {
+            profile.SESSDATA = ''
+            profile.biliJct = ''
             let obj = {}
             obj.code = 0
             profile.platform = 'bilibili'
@@ -23,14 +39,12 @@ let getUserInfo = async (ctx, next) => {
             profile.status = 1
             obj.data = [profile]
             ctx.body = obj
-            await next()
         }
         else {
             let obj = {}
             obj.code = 0
             obj.data = profile
             ctx.body = obj
-            await next()
         }
     }
 }

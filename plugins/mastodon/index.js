0bdcc2dcb739ecf75e43fb743855b6be19e7bd1b
Ayaka Neko <neko@ayaka.moe>
2020-12-16 19:00:07 +0800

    :sparkles: 添加了 Mastodon 基本绑定逻辑
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- plugins/mastodon/index.js
+++ plugins/mastodon/index.js
@@ -1,18 +1,112 @@
 // Dependecies
 const axios = require('axios').default
+const crypto = require('crypto')
 let Compo = require('../../plugins')
 
 // Local Packages
 const Log = require('../../src/util/log')
 const Hash = require('../../src/util/hash')
 const Store = require('../../src/store/store')
+const { disassemble } = require('../../src/util/cookie')
+
+const passcode = require('../../encrypt.json')
+
+const algorithm = 'aes-256-ctr'
+
+const key = passcode.code
+let iv = passcode.iv
+
+function encryptText (text) {
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
+  let encrypted = cipher.update(text)
+  encrypted = Buffer.concat([encrypted, cipher.final()])
+  return encrypted.toString('hex')
+}
+
+function decryptText (text) {
+  iv = Buffer.from(iv)
+  const encryptedText = Buffer.from(text, 'hex')
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
+  let decrypted = decipher.update(encryptedText)
+  decrypted = Buffer.concat([decrypted, decipher.final()])
+  return decrypted.toString()
+}
+
+const timeoutIds = new Map()
 
 // Plugin Method
 // Inner
 
 exports.endpoints = {
-    main: async () => {
+    getToken: async (ctx) => {
+        const authTokenRaw = ctx.request.headers.authorization
+        const authToken = authTokenRaw.replace(/^Bearer./, '')
 
+        const res = disassemble(authToken)
+        const oauthKeyExist = await Store.main.find({ key: 'MastodonOAuthKey', id: res.id })
+        
+        if (oauthKeyExist) await Store.main.remove({ key: 'MastodonOAuthKey', id: res.id }, {})
+
+        const timestamp = (new Date()).getTime()
+        const oauthKey = encryptText(res.id + "" + timestamp)
+        const oauthKeyUUID = Hash.md5(res.id + "" + timestamp)
+
+        await Store.main.insert({ key: 'MastodonOAuthKey', id: res.id, token: oauthKey, uuid: oauthKeyUUID, timestamp: timestamp })
+
+        const id = setTimeout(() => {
+            Store.main.remove({ key: 'MastodonOAuthKey', id: res.id }, {})
+        }, 6000)
+
+        timeoutIds.set(res.id + "", id)
+
+        const oauth = await Store.main.findOne({ key: 'MastodonOAuthKey', id: res.id, token: oauthKey, uuid: oauthKeyUUID })
+        console.log(oauth)
+        ctx.body = {
+            code: 0,
+            token: oauth.token
+        }
+    },
+    getAuthorizeToken: async (ctx) => {
+        let query = ctx.request.query
+        query = JSON.parse(JSON.stringify(query))
+        
+        const oauthTokenRaw = ctx.request.headers.authorization
+        const oauthToken = oauthTokenRaw.replace(/^Bearer./, '')
+
+        const res = await Store.main.findOne({ key: 'MastodonOAuthKey', id: parseInt(query.id) })
+        if (res) {
+            clearTimeout(timeoutIds.get(query.id + ""))
+            timeoutIds.delete(query.id + "")
+            await Store.main.remove({ key: 'MastodonOAuthKey', id: parseInt(query.id) }, {})
+        }
+        
+        if (Hash.md5(decryptText(oauthToken)) === res.uuid) {
+            const userExist = await Store.user.findOne({ key: "UserMastodonProfile", id: query.id })
+            if (userExist) {
+                await Store.user.update({ key: "UserMastodonProfile", id: query.id }, { $set: { userId: query.userId }}, {})
+                await Store.user.update({ key: "UserMastodonProfile", id: query.id }, { $set: { domain: query.domain }}, {})
+                await Store.user.update({ key: "UserMastodonProfile", id: query.id }, { $set: { username: query.username} }, {})
+            }
+            else {
+                await Store.user.insert({ key: "UserMastodonProfile", id: query.id, userId: query.userId, domain: query.domain, username: query.username })
+            }
+            const user = await Store.user.findOne({ key: "UserMastodonProfile", id: query.id })
+            if (user) {
+                ctx.body = {
+                    code: 0,
+                    message: "success"
+                }
+            }
+            else {
+                ctx.body = {
+                    code: 1,
+                    message: "failed"
+                }
+            }
+        }
+        else {
+            ctx.status = 403
+        }
     }
 }
 
@@ -21,8 +115,13 @@ exports.endpoints = {
 exports.register = {
     endpoints: [
         {
-            function: 'main',
-            endpoint: '/oauth',
+            function: 'getToken',
+            endpoint: '/oauth/token',
+            method: 'GET'
+        },
+        {
+            function: 'getAuthorizeToken',
+            endpoint: '/oauth/authorize',
             method: 'GET'
         }
     ],

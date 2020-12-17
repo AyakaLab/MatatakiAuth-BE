b6e1a831148ae82fb0564e48c8b7a359408e9f18
Ayaka Neko <neko@ayaka.moe>
2020-12-17 15:26:15 +0800

    :zap: 同时兼容不同数据
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -29,11 +29,18 @@ let getUserInfo = async (ctx, next) => {
     
     if (!profile) ctx.body = { code: 0, message: 'User Not Found' }
     else {
+        let account = ''
+        if (profile.mainInfo) {
+            account = profile.mainInfo
+        }
+        else {
+            account = profile.userId
+        }
         if (query.platform) {
             let obj = {}
             obj.code = 0
             profile.platform = query.platform.toLowerCase()
-            profile.account = profile.mainInfo
+            profile.account = account
             profile.status = 1
             obj.data = [profile]
             ctx.body = obj
@@ -42,7 +49,7 @@ let getUserInfo = async (ctx, next) => {
             let obj = {}
             obj.code = 0
             obj.data = profile
-            profile.account = profile.mainInfo
+            profile.account = account
             ctx.body = obj
         }
     }
